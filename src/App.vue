ed7f6b96cbb88e13a8dbb58b371cd788ec55aabb
Progammer <programmer@nbadigitalworlds.com>
2023-10-04 20:14:52 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app" >
+  <div id="app">
     <!-- <img alt="Vue logo" src="./assets/img/banner2.jpg"> -->
-    <HomeView/>
+    <HomeView />
   </div>
 </template>
 
@@ -10,6 +10,9 @@ import HomeView from './views/HomeView.vue'
 
 export default {
   name: 'App',
+  async created() {
+    document.title = 'Dekrub Shop';
+  },
   components: {
     HomeView
   }
@@ -18,6 +21,7 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@100;200;300&display=swap');
+
 #app {
   font-family: 'Kanit', sans-serif !important;
 }
